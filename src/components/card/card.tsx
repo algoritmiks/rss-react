29b49fcfb528c480e12a0c4d4afe0f5016ea5b3e
Aleksei Osipov <alexey11rus@gmail.com>
2024-07-12 21:28:31 +0300

    card styles

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -15,8 +15,9 @@ export const Card: React.FC<{ user: IUser }> = ({ user }) => {
         to={`/user/${user.id}?page=${pageParam}&search=${searchParam}`}
       >
         <div className={css.info}>
-          <div className={css.name}>
-            {user.firstName} {user.lastName}
+          <div className={css.nameContaner}>
+            <div className={css.name}>{user.firstName}</div>
+            <div className={css.name}>{user.lastName}</div>
           </div>
           <p>Age: {user.age}</p>
         </div>
