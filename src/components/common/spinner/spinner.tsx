3ee72df7ca41ecce6df1bdf368f6b993f37359f4
Aleksei Osipov <alexey11rus@gmail.com>
2024-07-13 14:39:31 +0300

    vitest add

--- src/components/common/spinner/spinner.tsx
+++ src/components/common/spinner/spinner.tsx
@@ -2,7 +2,7 @@ import css from './spinner.module.css'
 
 export const Spinner: React.FC = () => {
   return (
-    <div className={css.container}>
+    <div className={css.container} role="container">
       <div className={css.spinner}></div>
     </div>
   )
