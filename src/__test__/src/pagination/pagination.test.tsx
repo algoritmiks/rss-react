d0d132a828e1e0d021985effdb86071b4e266529
Aleksei Osipov <alexey11rus@gmail.com>
2024-07-25 14:55:26 +0300

    pagination test change

--- src/__test__/src/pagination/pagination.test.tsx
+++ src/__test__/src/pagination/pagination.test.tsx
@@ -1,19 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import { render, screen } from '@testing-library/react'
-import { vi, describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import '@testing-library/jest-dom'
 import { Pagination } from '../../../components/pagination/pagination'
+import { Provider } from 'react-redux'
+import { store } from '../../../store/store'
 
 describe('Pagination component', () => {
   it('renders buttons in pagination', async () => {
-    const setCurrentPage = vi.fn()
-
     render(
-      <Pagination
-        currentPage={1}
-        totalPages={1}
-        setCurrentPage={setCurrentPage}
-      />,
+      <Provider store={store}>
+        <Pagination />
+      </Provider>,
       {
         wrapper: BrowserRouter,
       },
