1309853e50e9c1395281d2a69fbe2213b76b71f6
Aleksei Osipov <alexey11rus@gmail.com>
2024-08-01 18:38:00 +0300

    ErrorBoundary wrapper add, _document add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../providers/themeProvider'
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
+import ErrorBoundary from '../components/errorBoundary/errorBoundary'
 import '../styles/index.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ErrorBoundary>
     </Provider>
   )
 }
---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <meta charSet="UTF-8" />
+      <title>react 2024Q3</title>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
