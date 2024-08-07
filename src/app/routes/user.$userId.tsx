a025a496463c08475daa99158906ddcc90913a13
Aleksei Osipov <alexey11rus@gmail.com>
2024-08-07 12:38:28 +0300

    console remove

--- src/app/routes/user.$userId.tsx
+++ src/app/routes/user.$userId.tsx
@@ -4,12 +4,7 @@ import { fetchUser } from '../../api/api'
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url)
-
-  console.log('url > ', url)
   const userId = url.pathname.match(/\/user\/(\d+)/)
-  if (userId) {
-    console.log('user id > ', userId[1])
-  }
   const data = await fetchUser(userId ? userId[1] : '')
   return data
 }
